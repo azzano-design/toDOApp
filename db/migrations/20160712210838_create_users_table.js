468f32215f0e2e2044e5222edd6b1540072892d6
Payal Dhiman <payaldhiman@outlook.com>
2017-12-22 03:02:14 +0000

    alter user table

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
-    // table.increments();
-    // table.string('firstname');
-    // table.string('lastname');
-    // table.string('email');
-    // table.string('password');
+    table.increments();
+    table.string('firstname');
+    table.string('lastname');
+    table.string('email');
+    table.string('password');
   });
 
 };
