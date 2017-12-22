47c97d9f46309e37c7c9d0155e2f394c76832da7
Brendan <brendanarchangelo@gmail.com>
2017-12-22 23:03:31 +0000

    fun features for todo items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,25 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+  $( function() {
+    $( "#accordion" ).accordion({
+      header: '.todo-list-item-header',
+      collapsible: true,
+      active: false
+    }).sortable({
+        axis: "y",
+        handle: "div",
+        stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          div.item.children( "div" ).triggerHandler( "focusout" );
+          // Refresh accordion to handle new order
+          $( this ).accordion( "refresh" );
+        }
+      });;
+    $('.todo-list-item-header').on( "click", 'input[type="checkbox"]', function(e) {
+      e.stopPropagation();
+      $(this).parent().parent().fadeOut();
+    });
+  } );
 });
